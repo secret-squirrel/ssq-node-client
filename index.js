67eb3310ae14d35cf96a33e05654da0e0b458dfc
Matt MacAulay <matt@dulcepericulum.org>
2014-04-01 08:35:19 -0400

    CLI cleanup

---
+++ index.js
@@ -0,0 +1,26 @@
+#!/usr/bin/env node
+
+var program = require('commander')
+var prompt = require('prompt')
+var pkg = require('./package.json')
+var app = require('./lib/squirrel')
+
+program
+  .version(pkg.version)
+  .option('--create-keypair', 'Generate a new keypair')
+  .option('--create-user', 'Create a new user')
+  .parse(process.argv)
+
+if(program.createKeypair) {
+  app.createKeyPair()
+}
+
+if(program.createUser) {
+  prompt.get(['firstName', 'lastName', 'email'], function(err, result) {
+    var firstName = program.firstName
+    var lastName = program.lastName
+    var email = program.email
+
+    app.createUser(firstName, lastName, email)
+  })
+}
