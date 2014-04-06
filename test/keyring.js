7c37387542e181953100ef3b8a470bf3940eb0df
Matt MacAulay <matt@dulcepericulum.org>
2014-04-06 14:24:36 -0400

    Cleaned up keyring code and tests

--- test/keyring.js
+++ test/keyring.js
@@ -2,34 +2,27 @@ var fs = require('fs')
 var path = require('path')
 var crypto = require('crypto')
 var ursa = require('ursa')
-var keyring = require('../keyring')
+var keyring = require('../lib/keyring')
+
+var passPhrase = 's00pers3krit'
+var cipher = 'aes-256-cbc'
+var bits = 2048
 
 describe('keyring', function() {
-  it('generates RSA keypairs', function() {
-    var keyPair = keyring.generateKeyPair(2048)
-
-    var privateKey = ursa.coercePrivateKey(keyPair.privateKey)
-    var publicKey = ursa.coercePublicKey(keyPair.publicKey)
-
-    assert(ursa.isPrivateKey(privateKey))
-    assert(ursa.isPublicKey(publicKey))
-  })
-
   it('creates a public/private keypair with the private key encrypted', function() {
-    var passPhrase = 's00pers3krit'
-    var keyPair = keyring.createKeyPair(passPhrase, 2048)
-    var decipher = crypto.createDecipher('aes-256-cbc', passPhrase)
-    var decrypted = decipher.update(keyPair.privateKey, 'binary')
-    decrypted += decipher.final('binary')
+    var keyPair = keyring.createKeyPair(passPhrase, cipher, bits)
 
-    var key = ursa.coercePrivateKey(decrypted)
-    assert(ursa.isPrivateKey(key), 'Return a URSA private key')
+    var publicKey = ursa.coercePublicKey(keyPair.publicKey)
+    assert(ursa.isPublicKey(publicKey), 'Return a ursa public key')
+
+    var privateKey = ursa.createPrivateKey(keyPair.privateKey, passPhrase, 'utf8')
+    assert(ursa.isPrivateKey(privateKey), 'Return a ursa private key')
   })
 
   it('saves keypairs to the config directory', function() {
     var configDir = path.join(process.cwd(), 'tmp/.squirrel')
 
-    var keyPair = keyring.generateKeyPair(2048)
+    var keyPair = keyring.createKeyPair(passPhrase, cipher, bits)
     keyring.saveToKeyRing(keyPair, configDir)
 
     assert(fs.existsSync(path.join(configDir, 'id_rsa')), 'Saves private key to disk')
