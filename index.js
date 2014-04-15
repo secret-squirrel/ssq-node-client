05e957f9c47a28d0fefb59ee51f62d2c7ecc84dd
Matt MacAulay <matt@dulcepericulum.org>
2014-04-15 08:45:58 -0400

    Refactoring cli to use commander's 'command' feature

--- index.js
+++ index.js
@@ -5,50 +5,57 @@ var prompt = require('prompt')
 var pkg = require('./package.json')
 var squirrel = require('./lib/squirrel')
 
+program.version(pkg.version)
+
 program
-  .version(pkg.version)
-  .option('--create-keypair', 'Generate a new keypair')
-  .option('--create-user', 'Create a new user')
-  .option('--get-users', 'Lists users')
-  .parse(process.argv)
-
-if(program.createKeypair) {
-  // TODO: validation. bits should be at least 2048, and should default to
-  // that. passphrase should be of a certain length.
-  var schema = {
-    properties: {
-      bits: {},
-      passPhrase: { hidden: true }
+  .command('create-keypair')
+  .description('Generate a new keypair')
+  .action(function() {
+    // TODO: validation. bits should be at least 2048, and should default to
+    // that. passphrase should be of a certain length.
+    var schema = {
+      properties: {
+        bits: {},
+        passPhrase: { hidden: true }
+      }
     }
-  }
-  prompt.get(schema, function(err, result) {
-    var bits = parseInt(result.bits)
-    var passPhrase = result.passPhrase
-    squirrel.createKeyPair(passPhrase, bits, function(err) {
-      if(err) throw err
-      console.log('Keypair saved.')
+    prompt.get(schema, function(err, result) {
+      var bits = parseInt(result.bits)
+      var passPhrase = result.passPhrase
+      squirrel.createKeyPair(passPhrase, bits, function(err) {
+        if(err) throw err
+        console.log('Keypair saved.')
+      })
     })
   })
-} else if(program.createUser) {
-  squirrel.getContext(getPassPhrase, function(err, context) {
-    console.log('\nCreating a new user.')
-    prompt.get(['name', 'email'], function(err, result) {
-      var name = result.name
-      var email = result.email
-      if(err) throw err
-      squirrel.createUser(context, name, email)
-    })
-  })
-} else if(program.getUsers) {
-  squirrel.getContext(getPassPhrase, function(err, context) {
-    if(err) throw err
-    squirrel.getUsers(context)
-  })
-}
 
-else{
-  program.help()
-}
+program
+  .command('create-user')
+  .description('Create a new user')
+  .action(function() {
+    squirrel.getContext(getPassPhrase, function(err, context) {
+      console.log('\nCreating a new user.')
+      prompt.get(['name', 'email'], function(err, result) {
+        var name = result.name
+        var email = result.email
+        if(err) throw err
+        squirrel.createUser(context, name, email)
+      })
+    })
+  })
+
+program
+  .command('list-users')
+  .description('Display a list of users in the system')
+  .action(function() {
+    squirrel.getContext(getPassPhrase, function(err, context) {
+      if(err) throw err
+      squirrel.getUsers(context)
+    })
+  })
+
+program.command('*').action(program.help)
+program.parse(process.argv)
 
 function getPassPhrase(callback) {
   var schema = {
